983cac86e5509fbcd2d7ef2f4b4f650d16425e20
Kuba <urbanski.jakub77@gmail.com>
2023-12-10 23:52:05 +0100

    fix(frontend): adjust movie adding form to Material UI

--- react/movies_with_neo4j/src/components/MovieForm.jsx
+++ react/movies_with_neo4j/src/components/MovieForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import apiClient from '../api/apiClient';
+import { TextField, Button, Paper, Box, FormControl } from '@mui/material';
 
 const MovieForm = () => {
   const [title, setTitle] = useState('');
@@ -8,7 +9,7 @@ const MovieForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await apiClient.post('/movie', { title, year });
+      const response = await apiClient.post('/movie', { title, year: Number(year) });
       console.log(response.data);
       setTitle('');
       setYear('');
@@ -18,22 +19,34 @@ const MovieForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Movie Title"
-        required
-      />
-      <input
-        type="number"
-        value={year}
-        onChange={(e) => setYear(e.target.value)}
-        placeholder="Release Year"
-      />
-      <button type="submit">Add new movie</button>
-    </form>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
+      <Paper elevation={3} sx={{ p: 3, minWidth: '300px' }}>
+        <FormControl fullWidth sx={{ mb: 2 }}>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              label="Movie Title"
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+              variant="outlined"
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              label="Release Year"
+              type="number"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+              variant="outlined"
+              sx={{ mb: 2 }}
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Add new movie
+            </Button>
+          </form>
+        </FormControl>
+      </Paper>
+    </Box>
   );
 };
 

e3f21b3a4589deb8c69120dcf4624a57a7fb2f6e
Kuba <urbanski.jakub77@gmail.com>
2023-12-12 17:47:24 +0100

    fixes

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_restful import Api
 from .resources.movie import MovieResource, SingleMovieResource
 from .resources.director import DirectorResource
 from .resources.directs import Directs, MovieDirectorResource, DirectorMoviesResource
 from flask_cors import CORS
+import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../../react/movies_with_neo4j/dist')
     api = Api(app)
     CORS(app)
 
@@ -19,5 +20,15 @@ def create_app():
     
     api.add_resource(MovieDirectorResource, '/movie/<string:movie_id>/director')
     api.add_resource(DirectorMoviesResource, '/director/<string:director_id>/movies')
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        if path != "" and os.path.exists(app.static_folder + '/' + path):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, 'index.html')
+
+    return app
 
     return app
